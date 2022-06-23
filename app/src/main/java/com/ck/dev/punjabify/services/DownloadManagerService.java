822f4b5933d61a6431990fda1b514a37403e378b
jashanpreet-singh-99 <jashan.singh.work@icloud.com>
2022-06-24 02:14:09 +0530

    FIX: Downloading of track and start of migration to fixed item
    Pending work only intermediate commit for transfer of data.

--- app/src/main/java/com/ck/dev/punjabify/services/DownloadManagerService.java
+++ app/src/main/java/com/ck/dev/punjabify/services/DownloadManagerService.java
@@ -17,6 +17,7 @@ import com.ck.dev.punjabify.R;
 import com.ck.dev.punjabify.model.DownloadData;
 import com.ck.dev.punjabify.model.ServerizedTrackData;
 import com.ck.dev.punjabify.threads.interfaces.UiTrackDownloadingThreadCallBack;
+import com.ck.dev.punjabify.threads.tasks.TrackUrlDownloader;
 import com.ck.dev.punjabify.utils.Config;
 import com.ck.dev.punjabify.utils.ServerizedManager;
 
@@ -27,10 +28,13 @@ import java.util.concurrent.Executors;
 
 public class DownloadManagerService extends Service implements UiTrackDownloadingThreadCallBack {
 
+    private UiTrackDownloadingThreadCallBack uiTrackDownloadingThreadCallBack = null;
     private ServerizedManager serverizedManager;
-    private ArrayList<DownloadData> downloadQueue = new ArrayList<>();
+    private final ArrayList<DownloadData> downloadQueue = new ArrayList<>();
 
     private ExecutorService executorService;
+
+    private ServerizedTrackData currentDownloadingTrack;
 
     private final IBinder iBinder = new LocalBinder();
 
@@ -63,18 +67,23 @@ public class DownloadManagerService extends Service implements UiTrackDownloadin
         if (executorService == null) {
             executorService = Executors.newSingleThreadExecutor();
         }
-
+        if (downloadQueue.size() > 0 ) {
+            Config.LOG(Config.TAG_DOWNLOAD, "Has some files to download.", false);
+            downloadFirstQueueTrack();
+            return START_STICKY;
+        }
+        this.stopSelf();
         return super.onStartCommand(intent, flags, startId);
     }
 
     private String createNotificationChannel() {
-        NotificationChannel chan;
+        NotificationChannel Channel;
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            chan = new NotificationChannel(Config.CHANNEL_ID, "Preparing Download Manager ", NotificationManager.IMPORTANCE_MIN);
-            chan.setLightColor( Color.BLUE);
-            chan.setLockscreenVisibility(Notification.VISIBILITY_PUBLIC);
+            Channel = new NotificationChannel(Config.CHANNEL_ID, "Preparing Download Manager ", NotificationManager.IMPORTANCE_MIN);
+            Channel.setLightColor( Color.BLUE);
+            Channel.setLockscreenVisibility(Notification.VISIBILITY_PUBLIC);
             NotificationManager service = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-            Objects.requireNonNull(service).createNotificationChannel(chan);
+            Objects.requireNonNull(service).createNotificationChannel(Channel);
             return Config.CHANNEL_ID;
         }
         return "None";
@@ -92,43 +101,63 @@ public class DownloadManagerService extends Service implements UiTrackDownloadin
         return downloadQueue;
     }
 
-    public void insertNewDownloads() {
-
+    public void clearDownloadQueue() {
+        if (serverizedManager != null) {
+            serverizedManager.clearDownloadQueue();
+        }
     }
 
-//    private void downloadFirstQueueTrack() {
-//        TrackUrlDownloader trackUrlDownloader = new TrackUrlDownloader();
-//        trackUrlDownloader.setMetaData(
-//                this,
-//                getCacheDir() + Config.TRACKS_DIR,
-//                downloadQueue.get(0).getTrackData()
-//        );
-//        if (executorService == null) {
-//            executorService = Executors.newSingleThreadExecutor();
-//        }
-//        executorService.submit(trackUrlDownloader);
-//        Config.LOG(Config.TAG_DOWNLOAD, "Starting to Download track present in queue.", false);
-//
-//    }
+    public void downloadFirstQueueTrack() {
+        TrackUrlDownloader trackUrlDownloader = new TrackUrlDownloader();
+        trackUrlDownloader.setMetaData(
+                this,
+                getCacheDir() + Config.TRACKS_DIR,
+                downloadQueue.get(0).getTrackData()
+        );
+        currentDownloadingTrack = downloadQueue.get(0).getTrackData();
+        if (executorService == null) {
+            executorService = Executors.newSingleThreadExecutor();
+        }
+        executorService.submit(trackUrlDownloader);
+        Config.LOG(Config.TAG_DOWNLOAD, "Starting to Download track present in queue.", false);
+    }
 
     @Override
     public void progressUpdated(int progress) {
-
+        uiTrackDownloadingThreadCallBack.progressUpdated(progress);
     }
 
     @Override
     public void trackDownloaded(int id) {
-
+        boolean work = serverizedManager.updateTrackToDownloaded(id);
+        if (work) {
+            serverizedManager.removeDownloadQueueTrack(id);
+            downloadQueue.remove(0);
+            uiTrackDownloadingThreadCallBack.trackDownloaded(id);
+        }
+        Config.LOG(Config.TAG_DOWNLOAD, "Track Downloaded To Local Dir " + work, false);
+        if (downloadQueue.size() > 0) {
+            downloadFirstQueueTrack();
+        }
     }
 
     @Override
     public void updateMax(int max) {
-
+        uiTrackDownloadingThreadCallBack.updateMax(max);
     }
 
     @Override
     public void alreadyDownloaded() {
+        uiTrackDownloadingThreadCallBack.alreadyDownloaded();
+    }
 
+    @Override
+    public void startTrackDownload(ServerizedTrackData track) {
+        uiTrackDownloadingThreadCallBack.startTrackDownload(track);
+    }
+
+    public void setCallBack(UiTrackDownloadingThreadCallBack uiTrackDownloadingThreadCallBack){
+        this.uiTrackDownloadingThreadCallBack = uiTrackDownloadingThreadCallBack;
     }
 
     public class LocalBinder extends Binder {
