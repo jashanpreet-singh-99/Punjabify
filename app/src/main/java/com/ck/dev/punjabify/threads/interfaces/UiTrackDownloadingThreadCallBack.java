5a8b1793364eead883bc4283840f773f050a9070
jashanpreet-singh-99 <jashan.singh.work@icloud.com>
2022-06-24 02:43:57 +0530

    FEAT: Added startTrack callback
    This callback help to notify when the download actually starts.

--- app/src/main/java/com/ck/dev/punjabify/threads/interfaces/UiTrackDownloadingThreadCallBack.java
+++ app/src/main/java/com/ck/dev/punjabify/threads/interfaces/UiTrackDownloadingThreadCallBack.java
@@ -1,8 +1,11 @@
 package com.ck.dev.punjabify.threads.interfaces;
 
+import com.ck.dev.punjabify.model.ServerizedTrackData;
+
 public interface UiTrackDownloadingThreadCallBack {
     void progressUpdated(int progress);
     void trackDownloaded(int id);
     void updateMax(int max);
     void alreadyDownloaded();
+    void startTrackDownload(ServerizedTrackData track);
 }
