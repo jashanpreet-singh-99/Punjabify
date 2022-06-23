6c12040e0c2ee02dc243f2c5c5d9b5ec5d2c790f
jashanpreet-singh-99 <jashan.singh.work@icloud.com>
2022-06-24 02:41:42 +0530

    FEAT: Added AppCheck
    Added the ability to send appCheck token with each firebase request.

--- app/src/main/java/com/ck/dev/punjabify/MasterApplication.java
+++ app/src/main/java/com/ck/dev/punjabify/MasterApplication.java
@@ -5,6 +5,9 @@ import android.content.IntentFilter;
 import android.net.ConnectivityManager;
 
 import com.ck.dev.punjabify.broadcast.InternetConnectionReceiver;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.appcheck.FirebaseAppCheck;
+import com.google.firebase.appcheck.safetynet.SafetyNetAppCheckProviderFactory;
 
 public class MasterApplication extends Application {
 
@@ -14,6 +17,11 @@ public class MasterApplication extends Application {
     public void onCreate() {
         super.onCreate();
         registerConnectivityReceiver();
+        FirebaseApp.initializeApp(this);
+        FirebaseAppCheck firebaseAppCheck = FirebaseAppCheck.getInstance();
+        firebaseAppCheck.installAppCheckProviderFactory(
+                SafetyNetAppCheckProviderFactory.getInstance());
+        firebaseAppCheck.setTokenAutoRefreshEnabled(true);
     }
 
     private void registerConnectivityReceiver() {
